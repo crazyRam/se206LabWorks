1c03b82c4bdaace7add874b0bca3a5d2195defbf
crazyRam <niyazi_oz@hotmail.com>
2013-08-02 10:41:51 +1200

    third change

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,8 @@
 def my_git_function():
     print("Hello World!")
+    print("This is my second line and commit")
+    for(i=0, i<5, i++)
+        print(i)
 
 if __name__ == "__main__":
     my_git_function()
