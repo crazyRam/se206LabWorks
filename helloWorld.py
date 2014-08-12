f7c331fed50bfa213160a8bbdaef79f85db16859
Niyazi Ozturk <nozt001@aucklanduni.ac.nz>
2014-08-12 17:44:35 +1200

    I have added a new line today

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,6 @@
 def my_git_function():
     print("Hello World!")
+    print("Niyazi this is my 13-8-2014 change")
     print("This is my second line and commit")
     for i in range(3):
         print(i)
